972dae52e561b63b88e81fee29f4e7d7b7ce987b
kaikoga <info@mail.kaikoga.net>
2025-08-16 22:26:27 +0900

    maybe ToonStandard (Outline)

--- QuestReplacer/Packages/net.kaikoga.questreplacer/Editor/Silksprite/QuestReplacer/External/Shaders.cs
+++ QuestReplacer/Packages/net.kaikoga.questreplacer/Editor/Silksprite/QuestReplacer/External/Shaders.cs
@@ -8,6 +8,7 @@ namespace Silksprite.QuestReplacer
         public static readonly Shader VrcMobileStandardLite = Shader.Find("VRChat/Mobile/Standard Lite");
         public static readonly Shader VrcMobileToonLit = Shader.Find("VRChat/Mobile/Toon Lit");
         public static readonly Shader VrcMobileToonStandard = Shader.Find("VRChat/Mobile/Toon Standard");
+        public static readonly Shader VrcMobileToonStandardOutline = Shader.Find("VRChat/Mobile/Toon Standard (Outline)");
         public static readonly Shader VrmMToon = Shader.Find("VRM/MToon");
         public static readonly Shader VrmMToon10 = Shader.Find("VRM10/MToon10");
     }
