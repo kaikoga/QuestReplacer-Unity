130a6f430209dbbe0f713afa3cc73b5bf8fdb870
kaikoga <info@mail.kaikoga.net>
2025-08-16 23:05:08 +0900

    break serialization once again

--- QuestReplacer/Packages/net.kaikoga.questreplacer/Runtime/Silksprite/QuestReplacer/QuestReplacerPlatform.cs
+++ QuestReplacer/Packages/net.kaikoga.questreplacer/Runtime/Silksprite/QuestReplacer/QuestReplacerPlatform.cs
@@ -4,13 +4,16 @@ namespace Silksprite.QuestReplacer
 {
     public enum QuestReplacerPlatform
     {
+        Generic = 0x00,
+        [InspectorName("VRChat PC")]
+        VRChatPC = 0x100,
         [InspectorName("VRChat Mobile")]
-        VRChatMobile = 0x00,
+        VRChatMobile = 0x110,
         [InspectorName("VRChat Android")]
-        VRChatAndroid = 0x01,
+        VRChatAndroid = 0x111,
         [InspectorName("VRChat iOS")]
-        VRChatIos = 0x02,
-        VRM0 = 0x10,
-        VRM1 = 0x11,
+        VRChatIos = 0x112,
+        VRM0 = 0x200,
+        VRM1 = 0x210,
     }
 }
