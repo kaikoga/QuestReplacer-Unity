c3dce157e9b26ce1f63591f07a8b0ec6d7246cf0
kaikoga <info@mail.kaikoga.net>
2025-08-17 10:17:07 +0900

    start being AdLib

--- QuestReplacer/Packages/net.kaikoga.questreplacer/Editor/Silksprite/QuestReplacer/Materials/MToonUpgrader.cs
+++ QuestReplacer/Packages/net.kaikoga.questreplacer/Editor/Silksprite/QuestReplacer/Materials/MToonUpgrader.cs
@@ -1,9 +1,25 @@
+using Silksprite.AdLib.Material.Access.Impl;
+using Silksprite.AdLib.Material.Impl;
+using UnityEngine;
+
 namespace Silksprite.QuestReplacer.Materials
 {
-    public class MToonUpgrader : SingleMaterialDuplicator
+    public class MToonUpgrader : ISingleMaterialDuplicator
     {
-        public MToonUpgrader() : base("VRM/MToon", Shaders.VrmMToon10)
+        public bool IsTarget(Material original) => original.shader.name == "VRM/MToon";
+
+        public Material Duplicate(Material original, string bakedAssetDirectoryPath)
         {
+            var mtoon = new MToonMaterialAccess(original.ToMaterialAccess());
+            var material = new Material(Shaders.VrmMToon10);
+            var mtoon10 = new MToon10MaterialAccess(material.ToMaterialAccess());
+            CopyMainTex(mtoon, mtoon10);
+            return material;
+        }
+
+        void CopyMainTex(MToonMaterialAccess mtoon, MToon10MaterialAccess mtoon10)
+        {
+            mtoon10.MainTex.Value = mtoon.MainTex.Value;
         }
     }
 }
