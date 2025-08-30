f0785934a41af11ffb9e76d1b73c0dd0a2c77f98
kaikoga <info@mail.kaikoga.net>
2025-08-30 22:19:23 +0900

    refactor lilToonToVRMMToon10

--- QuestReplacer/Packages/net.kaikoga.questreplacer/Editor/Silksprite/QuestReplacer/MaterialsExt/lilToon/lilToonToVRMMaterialDuplicatorMToon10.cs
+++ QuestReplacer/Packages/net.kaikoga.questreplacer/Editor/Silksprite/QuestReplacer/MaterialsExt/lilToon/lilToonToVRMMaterialDuplicatorMToon10.cs
@@ -1,36 +1,32 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Silksprite.QuestReplacer.Assets;
 using UnityEngine;
-
-#if QUESTREPLACER_VRMSHADERS || QUESTREPLACER_UNIVRM_WITH_SHADERS
-using Silksprite.QuestReplacer.MaterialsExt.VRMShaders;
-#endif
 
 namespace Silksprite.QuestReplacer.MaterialsExt.lilToon
 {
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     public class lilToonToVRMMaterialDuplicatorMToon10 : ISingleAssetDuplicator<Material>
     {
-        static readonly ISingleAssetDuplicator<Material> _duplicatorMToon = new lilToonToVRMMaterialDuplicator();
+        static readonly ISingleAssetDuplicator<Material>[] _duplicators =
+        {
+            new lilToonToVRMMaterialDuplicator(),
 #if QUESTREPLACER_VRMSHADERS || QUESTREPLACER_UNIVRM_WITH_SHADERS
-        static readonly ISingleAssetDuplicator<Material> _mToonUpgrader = new MToonUpgrader();
+            new VRMShaders.MToonUpgrader(),
 #endif
+        };
+
         int ISingleAssetDuplicator<Material>.Priority => 10000;
 
         bool ISingleAssetDuplicator<Material>.IsTarget(Material original)
         {
-            return _duplicatorMToon.IsTarget(original);
+            var originalShaderName = original.shader.name;
+            return originalShaderName.Contains("lilToon") || originalShaderName.StartsWith("Hidden/lts");
         }
 
         Material ISingleAssetDuplicator<Material>.Duplicate(Material original, string bakedAssetDirectoryPath)
         {
-            var mtoon = _duplicatorMToon.Duplicate(original, bakedAssetDirectoryPath);
-#if QUESTREPLACER_VRMSHADERS || QUESTREPLACER_UNIVRM_WITH_SHADERS
-            var material = _mToonUpgrader.Duplicate(mtoon, bakedAssetDirectoryPath);
-            return material;
-#else
-            return mtoon;
-#endif
+            return _duplicators.Aggregate(original, (material, duplicator) => duplicator.Duplicate(material, bakedAssetDirectoryPath));
         }
     }
 }
