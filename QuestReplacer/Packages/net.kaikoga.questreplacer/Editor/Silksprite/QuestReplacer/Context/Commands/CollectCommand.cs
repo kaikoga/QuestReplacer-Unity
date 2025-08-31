2fb7a207419c80f251ab6252a66a219352134d3d
kaikoga <info@mail.kaikoga.net>
2025-08-31 16:56:45 +0900

    exclude proxy animators

--- QuestReplacer/Packages/net.kaikoga.questreplacer/Editor/Silksprite/QuestReplacer/Context/Commands/CollectCommand.cs
+++ QuestReplacer/Packages/net.kaikoga.questreplacer/Editor/Silksprite/QuestReplacer/Context/Commands/CollectCommand.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Silksprite.QuestReplacer.Context.Commands
@@ -13,7 +15,14 @@ namespace Silksprite.QuestReplacer.Context.Commands
 
         protected override void DoExecute()
         {
-            QuestReplacer.AddEntries(Context.DeepCollectReferences<T>(), null, true);
+            var references = Context.DeepCollectReferences<T>();
+#if QUESTREPLACER_VRCSDK3_AVATARS
+            if (references is IEnumerable<AnimationClip> clips)
+            {
+                references = clips.Where(clip => !clip.name.StartsWith("proxy_")).OfType<T>();
+            }
+#endif
+            QuestReplacer.AddEntries(references, null, true);
         }
     }
 }
