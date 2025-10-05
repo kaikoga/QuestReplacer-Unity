edd155f7301fb3d9099a60c18067688629403b4a
kaikoga <info@mail.kaikoga.net>
2025-10-05 13:39:15 +0900

    this improves database integrity

--- QuestReplacer/Packages/net.kaikoga.questreplacer/Editor/Silksprite/QuestReplacer/Context/Commands/CommandBase.cs
+++ QuestReplacer/Packages/net.kaikoga.questreplacer/Editor/Silksprite/QuestReplacer/Context/Commands/CommandBase.cs
@@ -46,7 +46,6 @@ namespace Silksprite.QuestReplacer.Context.Commands
         public void Execute()
         {
             Undo.SetCurrentGroupName(Name);
-            Undo.RecordObject(QuestReplacer, Name);
             DoExecute();
             if (InvalidateTypeFilters)
             {
@@ -56,6 +55,7 @@ namespace Silksprite.QuestReplacer.Context.Commands
 
         internal void ExecuteWithoutUndo()
         {
+            Undo.RecordObject(QuestReplacer, Name);
             DoExecute();
         }
 
